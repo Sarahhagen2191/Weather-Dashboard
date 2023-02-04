22043a96aa4b1f9ced9adf62ec0b12f13c83775a
Sarah hagen <sarahhagen@192.168.1.245>
2023-02-04 21:34:59 +0000

    update css and add boxes

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Define all global variables
+var apiKey = '3f010404cfd18d774221892b08e82f52';
+var baseURL = "https://api.openweathermap.org/data/2.5/";
+var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
+var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
+var iconURL = "https://openweathermap.org/img/w/";
+var 
+
